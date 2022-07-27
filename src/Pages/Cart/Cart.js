5547301d412b326e80c602b91f5c88a79be40c83
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-07-27 22:38:37 +0600

    implementing cart

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 
 export default class Cart extends Component {
-    
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
   render() {
     return (
       <div>
-        <div>My Bag: {this.props.length} items</div>
+        <div>My Bag: {this.props.getdata.length} items</div>
         {
         this.props.getdata.length?
         <div>
-            <div>{this}</div>
+            {
+                this.props.getdata.map((element=>{
+                    return(
+                        <div>
+                            <div>{element.name}</div>
+                            <div>{element.prices[0].amount}</div>
+                            <div>{element.attributes[0].name}: {
+                            element.attributes[0].items.map(size=>{
+                                return(
+                                    <div>{size.value}</div>
+                                )
+                            })
+                            }</div>
+                        </div>
+                    )
+                }))
+            }
         </div>
         :
         <div>Cart is empty</div>
